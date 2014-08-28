9cf3605ed1476f60174a2f11c09bb74230df4ad6
jay <jaybamimore@hotmail.com>
2014-08-28 11:31:38 -0700

    a whole bunch of new stuff.. hope it works :)

--- app.js
+++ app.js
@@ -2,23 +2,31 @@
 * Module dependencies
 */
 var express = require('express')
-  , path = require('path');
+  , path = require('path')
+  , nib = require('nib');
 
 
 var app = express();
+/*
+function compile(str, path) {
+  return stylus(str)
+    .set('filename', path)
+    .set('compress', true)
+    .use(nib());
+}*/
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
-
+//app.use(require('stylus').middleware(__dirname + '/public'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 require('./routes/routes')(app);
 
 app.get('*', function(req, res){
-res.redirect('/'); 
+res.redirect('/'); // Implement a proper 404 page
 });
 
 app.get('/', function (req, res) {
