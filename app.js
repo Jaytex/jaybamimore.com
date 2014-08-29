26c020807058293ac6dc9d528b97e9c16c31a092
jay <jaybamimore@hotmail.com>
2014-08-29 15:27:43 -0700

    removed the unecessary files, added a music track, and updated the mini resume

--- app.js
+++ app.js
@@ -1,26 +1,17 @@
-/*
-* Module dependencies
-*/
+/* Module dependencies*/
 var express = require('express')
   , path = require('path')
   , nib = require('nib');
 
 
 var app = express();
-/*
-function compile(str, path) {
-  return stylus(str)
-    .set('filename', path)
-    .set('compress', true)
-    .use(nib());
-}*/
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
-//app.use(require('stylus').middleware(__dirname + '/public'));
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 require('./routes/routes')(app);
